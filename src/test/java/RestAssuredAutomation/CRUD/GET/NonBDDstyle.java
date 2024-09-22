cb18379f51a0ec46c5bc3efd1f268c947e14ac6a
ahmadarman532@gmail.com <ahmadarman532@gmail.com>
2024-09-23 04:28:52 +0530

    2nd Commit

---
+++ src/test/java/RestAssuredAutomation/CRUD/GET/NonBDDstyle.java
@@ -0,0 +1,24 @@
+package RestAssuredAutomation.CRUD.GET;
+
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+
+public class NonBDDstyle {
+   static RequestSpecification r = RestAssured.given();
+
+    public static void main(String[] args) {
+        r.baseUri("https://api.zippopotam.us");
+        test_bdd_2();
+
+    }
+    public static void test_bdd_2(){
+        r.basePath("/IN/-1");
+        r.when().get();
+        r.then().log().all().statusCode(400);
+    }
+    public static void test_bdd_3(){
+        r.basePath("/IN/211012");
+        r.when().get();
+        r.then().log().all().statusCode(201);
+    }
+}
